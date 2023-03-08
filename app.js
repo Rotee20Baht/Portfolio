a96853e174045bd9c39d878568993b878bdf48df
Rotee20Baht <dodohothot@hotmail.co.th>
2023-03-08 22:16:08 +0700

    Update some animation

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar');
 const navlink = document.querySelectorAll('.nav-link');
-// console.log(navlink);
+const skillbars = document.querySelectorAll(".skill-level-bar");
+const fadeTops = document.querySelectorAll(".fade-top");
+const fadeLefts = document.querySelectorAll(".fade-left");
+const fadeRights = document.querySelectorAll(".fade-right");
+const fadeBottom = document.querySelectorAll(".fade-bottom");
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        entry.isIntersecting ? entry.target.classList.add('active') : entry.target.classList.remove('active');
+    })
+})
+
+fadeTops.forEach(item => observer.observe(item))
+fadeLefts.forEach(item => observer.observe(item))
+fadeRights.forEach(item => observer.observe(item))
+fadeBottom.forEach(item => observer.observe(item))
+skillbars.forEach(bar => observer.observe(bar))
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
